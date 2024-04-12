7020cd12f29f43b566086724e7d17d2a90448970
Beatriz Newman <102912868+beatriznewman@users.noreply.github.com>
2024-04-12 11:36:31 -0300

    Add files via upload
    isso aqui é dentro do migrations

---
+++ 2024_04_12_140942_alunos_professores.php
@@ -0,0 +1,59 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        <?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alunos_professores', function (Blueprint $table) {
+            $table->foreignId('id_aluno')->constrained();
+            $table->foreignId('id_professor')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alunos_professores');
+    }
+};
+    }
+};
